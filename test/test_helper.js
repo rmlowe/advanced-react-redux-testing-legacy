0ca275ea90e5f37d22fbbeef102ca3f66cc668d0
Robert Lowe <robertmlowe@rmlowe.com>
2018-10-28 22:40:25 +0800

    Defining RenderComponent

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,16 +1,25 @@
-import jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
 import jquery from 'jquery';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';  // https://github.com/RealOrangeOne/react-native-mock/issues/152
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 // Set up testing environment to run like a browser in the command line
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
+global.window = (new JSDOM('<!doctype html><html><body></body></html>').window);  // https://github.com/airbnb/enzyme/issues/942
+global.document = window.document;
 const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers)}>
+      <ComponentClass />
+    </Provider>
+  );
 
   return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
@@ -20,3 +29,5 @@ function renderComponent(ComponentClass) {
 
 
 // Set up chai-jquery
+
+export { renderComponent, expect };
