645c4fdfb0bab625b23650307db75e3967071b17
Robert Lowe <robertmlowe@rmlowe.com>
2018-10-29 00:00:52 +0800

    Test Helper Review

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,11 +2,12 @@ import { JSDOM } from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-dom/test-utils';  // https://github.com/RealOrangeOne/react-native-mock/issues/152
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
 // Set up testing environment to run like a browser in the command line
 global.window = (new JSDOM('<!doctype html><html><body></body></html>').window);  // https://github.com/airbnb/enzyme/issues/942
@@ -26,12 +27,14 @@ function renderComponent(ComponentClass, props, state) {
 
 // Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
-
+  if (value) {
+    this.val(value);
+  }
   TestUtils.Simulate[eventName](this[0]);
 }
 
 
 // Set up chai-jquery
-
+chaiJquery(chai, chai.util, $);
 
 export { renderComponent, expect };
